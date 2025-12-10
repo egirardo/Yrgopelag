73cc9bbd1e69185ea7c2b1a72ffd86030a8084fd
Elsa Girardo <egirardo@hotmail.com>
2025-12-10 14:17:12 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . "app/views/header.php";
+
+?>
+
+
+
+<?php
+
+require __DIR__ . "app/views/footer.php";
+
+?>
---
+++ views/header.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__ . "app/autoload.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sporgle's Luxury Hotel</title>
+</head>
+
+<body>
+    <?php require __DIR__ . "app/views/navigation.php"; ?>

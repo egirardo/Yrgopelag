3932a1751192581e6ed1d48b0c89157112797191
Elsa Girardo <egirardo@hotmail.com>
2026-01-06 20:49:47 +0100

    Updated file paths in footer so that script loads on one.com

--- views/footer.php
+++ views/footer.php
@@ -1,9 +1,8 @@
 <footer></footer>
 
-<script src="/assets/scripts/script.js"></script>
-<script src="/assets/scripts/transfercode.js"></script>
-<script src="/assets/scripts/booking-calculator.js"></script>
-<script src="/assets/scripts/script.js"></script>
+<script src="assets/scripts/script.js"></script>
+<script src="assets/scripts/transfercode.js"></script>
+<script src="assets/scripts/booking-calculator.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" defer></script>
 </body>
 

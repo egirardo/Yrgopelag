c9928037b8143c7ab4e64a5f5e3864d9112d6017
Elsa Girardo <egirardo@hotmail.com>
2026-01-06 14:51:13 +0100

    Updated file paths for styling issues with one.com

--- index.php
+++ index.php
@@ -4,28 +4,45 @@
 <?php $rooms = getAllRooms($db);  ?>
 <div class="hero">
     <div class="hero-image">
-        <img src="/assets/images/hero.png" alt="beautiful luxury resort in forest">
+        <img src="assets/images/hero.png" alt="beautiful luxury resort in forest">
     </div>
-    <div class="review-slider-container">
-        <div class="review-slides-wrapper">
-            <div class="review-slides-inner">
-                <div class="review-card">
-                    <p class="review-text">"A fantastic place to stay! My kids loved the theme park and our family had a wonderful time at Sbargle's."</p>
-                    <p class="author-name">Samantha Pritchard</p>
-                </div>
-                <div class="review-card">
-                    <p class="review-text">"Sbargle was a gracious host and you can tell that he really cares for his guests."</p>
-                    <p class="author-name">Demi Moore</p>
-                </div>
-                <div class="review-card">
-                    <p class="review-text">"I really loved the casino. Beach by day, casino at night, what could be better?"</p>
-                    <p class="author-name">Stephen Thomas</p>
+    <div class="review-buttons-container">
+        <div class="review-slider-container">
+            <div class="review-slides-wrapper">
+                <div class="review-slides-inner">
+                    <div class="review-card">
+                        <p class="review-text">"A fantastic place to stay! My kids loved the theme park and our family had a wonderful time at Sbargle's."</p>
+                        <p class="author-name">Samantha Pritchard</p>
+                    </div>
+                    <div class="review-card">
+                        <p class="review-text">"Sbargle was a gracious host and you can tell that he really cares for his guests."</p>
+                        <p class="author-name">Demi Moore</p>
+                    </div>
+                    <div class="review-card">
+                        <p class="review-text">"I really loved the casino. Beach by day, casino at night, what could be better?"</p>
+                        <p class="author-name">Stephen Thomas</p>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="button-container">
+            <a href="about.php"><button class="btn btn-info">Learn More</button></a>
+            <a href="#booking-section"><button class="btn btn-primary">Book Your Stay</button></a>
+        </div>
     </div>
 </div>
-<div class="booking-title">
+<section class="overview">
+    <div class="the-resort">
+        <h3>The Resort</h3>
+        <p>Located on the island of Humanitopia, Sbargle's Luxury Building is a resort that offers guests a variety of stay options and several activities that humans enjoy. With beachfront views, Sbargle's is a paradise sure to bring you relaxation and comfort. Designed for year-round escapes, the temperature on the island is on average a comfortable 24°, perfect for swimming in the ocean or enjoying our amenities no matter the season.</p>
+    </div>
+</section>
+<section class="on-offer">
+    <div class="activities">
+
+    </div>
+</section>
+<div class="booking-title" id="booking-section">
     <h1 class="title">Book Your Stay Now!</h1>
 </div>
 <div class="rooms-display">
--- views/header.php
+++ views/header.php
@@ -7,13 +7,13 @@
     <title><?= $config['title']; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="/assets/styles/custom.css">
-    <link rel="stylesheet" href="/assets/styles/base.css">
-    <link rel="stylesheet" href="/assets/styles/layout.css">
-    <link rel="stylesheet" href="/assets/styles/components.css">
-    <link rel="stylesheet" href="/assets/styles/rooms.css">
-    <link rel="stylesheet" href="/assets/styles/booking.css">
-    <link rel="stylesheet" href="/assets/styles/reviews.css">
+    <link rel="stylesheet" href="assets/styles/custom.css">
+    <link rel="stylesheet" href="assets/styles/base.css">
+    <link rel="stylesheet" href="assets/styles/layout.css">
+    <link rel="stylesheet" href="assets/styles/components.css">
+    <link rel="stylesheet" href="assets/styles/rooms.css">
+    <link rel="stylesheet" href="assets/styles/booking.css">
+    <link rel="stylesheet" href="assets/styles/reviews.css">
 </head>
 
 <body data-bs-theme="dark">
